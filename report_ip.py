2ae27f4ecee695bfea12540f0a81dd8db795c41a
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2019-04-03 23:08:36 +0800

    modularization

--- report_ip.py
+++ report_ip.py
@@ -8,26 +8,20 @@ from email.utils import parseaddr, formataddr
 
 import smtplib
 
-def _format_addr(s):
+def format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
-from_addr = input('From: ')
-sender = input('Sender: ')
-password = input('Password: ')
-to_addr = input('To: ')
-receiver = input('Receiver: ')
-subject = input('Subject: ')
-smtp_server = input('SMTP server: ')
+def send_email(SMTP_host, account, password, From, To, to_addr, subject, content):
+    msg = MIMEText(content, 'plain', 'utf-8')
+    msg['From'] = format_addr('%s <%s>' % (From, account))
+    msg['To'] = format_addr('%s <%s>' % (To, to_addr))
+    msg['Subject'] = Header(subject, 'utf-8').encode()
+    email_client = smtplib.SMTP(SMTP_host)
+    email_client.starttls()
+    email_client.set_debuglevel(1)
+    email_client.login(account, password)
+    email_client.sendmail(account, [to_addr], msg.as_string())
+    email_client.quit()
 
-msg = MIMEText('hello, sent by Python...', 'plain', 'utf-8')
-msg['From'] = _format_addr('%s <%s>' % (sender, from_addr))
-msg['To'] = _format_addr('%s <%s>' % (receiver, to_addr))
-msg['Subject'] = Header(subject, 'utf-8').encode()
-
-server = smtplib.SMTP(smtp_server, 25)
-server.starttls()
-server.set_debuglevel(1)
-server.login(from_addr, password)
-server.sendmail(from_addr, [to_addr], msg.as_string())
-server.quit()
+#send_email(smtp_server, from_addr, password, sender, receiver, to_addr, subject, 'This e-mail is sent by Python...')
