cbeba2c2f517116455c24d7bfc96abdb65e506e1
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2019-04-04 16:59:18 +0800

    make script more strong

--- report_ip.py
+++ report_ip.py
@@ -9,7 +9,7 @@ from email.utils import parseaddr, formataddr
 import smtplib
 
 import os
-
+import socket
 import time
 
 def format_addr(s):
@@ -23,12 +23,15 @@ def send_email(smtp_server, from_addr, From, to_addr, To, subject, content, pass
     msg['To'] = format_addr('%s <%s>' % (To, to_addr))
     msg['Subject'] = Header(subject, 'utf-8').encode()
     # Send e-mail
-    email_client = smtplib.SMTP(smtp_server)
-    email_client.starttls()
-    email_client.set_debuglevel(1)
-    email_client.login(from_addr, password)
-    email_client.sendmail(from_addr, [to_addr], msg.as_string())
-    email_client.quit()
+    email_client = smtplib.SMTP(smtp_server, 25, 'Winterfell', 30)
+    # Try to send e-mail, if not successful, do not forget to close session
+    try:
+        email_client.starttls()
+        email_client.set_debuglevel(1)
+        email_client.login(from_addr, password)
+        email_client.sendmail(from_addr, [to_addr], msg.as_string())
+    finally:
+        email_client.quit()
 
 with open('config', 'r') as f:
     smtp_server = f.readline().strip()
@@ -39,6 +42,8 @@ with open('config', 'r') as f:
     subject = f.readline().strip()
     curl_cmd = f.readline().strip()
     nr_entries = int(f.readline().strip())
+    retry_interval = int(f.readline().strip())
+    max_retry = int(f.readline().strip())
 
 print('smtp_server:', smtp_server)
 print('from_addr:', from_addr)
@@ -49,21 +54,37 @@ print('Subject:', subject)
 password = input('Password for smtp server: ')
 print('Number of entries:', nr_entries)
 print('curl command for getting public IP address: \'%s\'' % curl_cmd)
-print()
+print('retry interval:', retry_interval)
+print('Max retry:', max_retry)
+print('########################################')
+print('## Script started:\n')
 
 seq_num = 0
 content = ['' for i in range(nr_entries + 2)]
 content[0] = 'Your IP change log:\n\n'
 content[-1] = '\nSent by Python\n'
-last_ip = ''
-current_ip = 'n/a'
+last_ip = 'n/a'
+current_ip = ''
 while True:
-    last_ip = current_ip
-    current_ip = os.popen(curl_cmd).readline().strip()
-    if current_ip != last_ip:
+    retry_count = 0
+    # Try to get current IP address
+    print('\n========================================')
+    print('Try to get IP address:\n')
+    while retry_count < max_retry:
+        try:
+            current_ip = os.popen(curl_cmd).readline().strip()
+            socket.inet_aton(current_ip)
+        except:
+            print('xxxxxxxxxxxxxxxxxxxx')
+            print('failed to get IP address')
+            retry_count += 1
+            time.sleep(retry_interval)
+        else:
+            break
+    if retry_count < max_retry and current_ip != last_ip:
         seq_num += 1
         timestamp = time.strftime('%Y-%m-%d %H:%M', time.localtime())
-        line = str('%s: %s -> %s\n' % (timestamp, last_ip, current_ip))
+        line = str('%s %s -> %s\n' % (timestamp, last_ip, current_ip))
         if content[seq_num] != '':
             content.pop(1)
             content.insert(nr_entries, line)
@@ -74,7 +95,22 @@ while True:
         ########## Test ##########
         print('----------')
         print(''.join(content))
-        print('==========')
+        print('----------')
         ########## End of test ##########
-        send_email(smtp_server, from_addr, From, to_addr, To, subject, ''.join(content), password)
-    time.sleep(60)
+        # Try to send e-mail
+        print('\n========================================')
+        print('Try to send e-mail:\n')
+        while retry_count < max_retry:
+            try:
+                send_email(smtp_server, from_addr, From, to_addr, To, subject, ''.join(content), password)
+            except:
+                print('an error occured during sending e-mail')
+                print('XXXXXXXXXXXXXXXXXXXX')
+                retry_count += 1
+                time.sleep(retry_interval)
+            else:
+                print('e-mail was sent successfully')
+                print('____________________')
+                last_ip = current_ip
+                break
+    time.sleep(retry_interval * (max_retry - retry_count))
